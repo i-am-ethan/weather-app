e868ba314200c0499d1ec32e89871a821c3d7efa
ethan <izawaryosuke0101@gmail.com>
2021-07-19 16:56:17 +0900

    fin

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,10 @@
-const Form = () => {
+const Form = (props) => {
+
     return(
         <form>
-            <input type="text" name="city" placeholder="都市名" />
-            <button type="submit">Get Weather</button>
+            <input type="text" name="city" placeholder="都市名" onChange={ e => props.setCity(e.target.value)} />
+            {/* {city} */}
+            <button type="submit" onClick={props.getWeather}>Get Weather</button>
         </form>
     )
 }
