181b7c8ecb0f97051d2723ff3df743fdf3a5ab47
ethan <izawaryosuke0101@gmail.com>
2021-07-19 17:37:27 +0900

    div解除

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,7 @@
 const Results = ({results}) => {
     const { cityName, country, temperature, conditionText, icon} = results;
     return(
-        <div>
+        <>
             {cityName && <div>{cityName}</div>}
             {country && <div>{country}</div>}
             {temperature && <div>{temperature}</div>}
@@ -9,7 +9,7 @@ const Results = ({results}) => {
                 <img src={icon} alt="icon" />
                 <span>{conditionText}</span>
             </div>}
-        </div>
+        </>
     )
 }
 export default Results;
